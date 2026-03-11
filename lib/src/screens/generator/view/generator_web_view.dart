75728f26d4690b91c727931fa6e711f230ff3318
Odialko <viktor1532@gmail.com>
2026-03-11 14:04:46 +0100

    implement web version

--- lib/src/screens/generator/view/generator_web_view.dart
+++ lib/src/screens/generator/view/generator_web_view.dart
@@ -1,16 +1,26 @@
+import 'package:color_generator/src/domains/constants/animation_constants.dart';
 import 'package:color_generator/src/screens/generator/generator_screen.dart';
+import 'package:color_generator/src/screens/generator/store/generator_store.dart';
+import 'package:color_generator/src/screens/generator/widgets/animated_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GeneratorWebView extends StatelessWidget implements GeneratorScreen {
+class GeneratorWebView extends ConsumerWidget implements GeneratorScreen {
   const GeneratorWebView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colorState = ref.watch(generatorProvider);
+    final colorNotifier = ref.read(generatorProvider.notifier);
+
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello there',
-          style: TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => colorNotifier.randomColorGenerator(),
+        child: AnimatedContainer(
+          duration: AnimationConstants.defaultAnimationDuration,
+          color: colorState.backgroundColor,
+          child: Center(child: AnimatedText()),
         ),
       ),
     );
